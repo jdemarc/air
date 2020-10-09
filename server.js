063864e6bd88b5ad8a4a4a664e21df9437ab26bf
joseph <josephjohndemarco@gmail.com>
2020-10-08 20:46:09 -0400

    Config database.

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const logger = require('morgan');
 
 const app = express();
 
+require('dotenv').config();
+require('./config/database');
+
+
 app.use(logger('dev'));
 app.use(express.json());
 
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api/users', require('./routes/api/users'));
 
 // Allow for proper client-side routing.
 app.get('/*', function(req, res) {
