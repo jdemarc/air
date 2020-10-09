28d31b7d661173c4d4967cd1337352dcdb8deba4
joseph <josephjohndemarco@gmail.com>
2020-10-09 11:46:33 -0400

    Connect to MongoDB Atlas.

--- config/database.js
+++ config/database.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect(process.env.DATABASE_URL, ...)
-mongoose.connect('mongodb://localhost/air', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 const db = mongoose.connection;
 
-db.on('connected', function () {
-    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
-})
+db.once('connected', () => {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
