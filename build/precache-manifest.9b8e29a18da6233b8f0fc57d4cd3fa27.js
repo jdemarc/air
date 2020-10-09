85f37659f5e1f6d5a972909f3570f5478ef010e3
joseph <josephjohndemarco@gmail.com>
2020-10-08 20:13:21 -0400

    Restructure project from class guidance.

---
+++ build/precache-manifest.9b8e29a18da6233b8f0fc57d4cd3fa27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60adc095c34b6c49bd3492573a238714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e41f6aaad0294e7d958",
+    "url": "/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "fe5797a356a4509855d6",
+    "url": "/static/js/2.a33913af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a33913af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e41f6aaad0294e7d958",
+    "url": "/static/js/main.ba8a5355.chunk.js"
+  },
+  {
+    "revision": "9eaf892834a3c103b908",
+    "url": "/static/js/runtime-main.3d20118c.js"
+  }
+]);
