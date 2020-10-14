adf586abc89a4a95268a4047bf138861cfd385ad
joseph <josephjohndemarco@gmail.com>
2020-10-14 12:31:10 -0400

    Style manage profile input boxes.

--- src/components/ProfileEditForm/index.jsx
+++ src/components/ProfileEditForm/index.jsx
@@ -7,7 +7,7 @@ const ProfileEditForm = ( {user} ) => {
     <div className="d-flex justify-content-center mt-5">
         <div className="card" style={{width: '30rem'}}>
           <h1 className="card-header text-center" id="title">Manage Profile</h1>
-          <div className="card-body d-flex align-items-start text-center">
+          <div className="card-body d-flex text-center">
             <div className="col-3">
               <svg width="3em" height="3em" viewBox="0 0 16 16" className="bi bi-person m-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
@@ -23,33 +23,34 @@ const ProfileEditForm = ( {user} ) => {
               </svg>
 
             </div>
-            <div className="col d-flex flex-column h-100">
-              <form className="">
-                <input className=""
+            <div className="d-flex flex-column">
+            <div className="col">
+                <input className="mt-4 mb-4"
                   placeholder="Display name"
                 />
 
-                <input className=""
+                <input className="mt-3 mb-3"
                   placeholder="Email"
                 />
 
-                <input className=""
+                <input className="mt-3"
                   placeholder="New password"
                 />
 
-                <input
+                <input className=""
                   placeholder="Confirm password"
                 />
-
-              </form>
             </div>
           </div>
+
+            </div>
               <div className="d-flex justify-content-around align-items-center m-3">
+                <button className="btn btn-primary">Submit</button>
+                
                 <Link to='/profile'>
                   <button className="btn btn-primary">Back</button>
                 </Link>
 
-                <button className="btn btn-primary">Submit</button>
               </div>
     </div>
 </div>
